7451cdffa1e6eaf917bff58f31375b5f02477b26
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-03 01:57:15 +0200

    Update src/CypherCommon/Tier1/CypherCommon_Vector.h

--- src/CypherCommon/Tier1/CypherCommon_Vector.h
+++ src/CypherCommon/Tier1/CypherCommon_Vector.h
@@ -1,3 +1,21 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  CypherEngine Source Code
+//  Copyright (c) 2026 Karlo Siric. All rights reserved.
+//
+//  File: src/CypherCommon/Tier1/CypherCommon_Vector.h
+//  Purpose: Declares CypherCommon Tier1 Vector support.
+//  Details: Tier1 builds practical utilities on top of Tier0 for strings, containers,
+//           parsing, data flow, and tool-facing helpers. Keep APIs explicit and
+//           stable because many systems will depend on them.
+//
+//  History:
+//  - Created by Karlo Siric on 2026-06-22
+//
+//  This file is proprietary and confidential. See LICENSE for details.
+//
+//////////////////////////////////////////////////////////////////////////
+
 #ifndef CYPHER_COMMON_TIER1_VECTOR_H
 #define CYPHER_COMMON_TIER1_VECTOR_H
 #pragma once
