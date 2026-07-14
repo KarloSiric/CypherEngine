fab46243276c030990f27b457d74b5c94ecaf332
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:56 +0200

    Wrap pragma once guard in CypherHost.h

--- src/CypherEngine/CypherHost/CypherHost.h
+++ src/CypherEngine/CypherHost/CypherHost.h
@@ -19,7 +19,9 @@
 #ifndef CYPHER_ENGINE_HOST_H
 #define CYPHER_ENGINE_HOST_H
 
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherHost_Error.h"
 #include "CypherHost_Types.h"
