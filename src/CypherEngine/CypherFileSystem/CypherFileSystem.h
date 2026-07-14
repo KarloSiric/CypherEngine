0daca08a939d4ca871521c5bc6c9c3bfd35c4ca0
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:56 +0200

    Wrap pragma once guard in CypherFileSystem.h

--- src/CypherEngine/CypherFileSystem/CypherFileSystem.h
+++ src/CypherEngine/CypherFileSystem/CypherFileSystem.h
@@ -19,7 +19,9 @@
 #ifndef CYPHER_ENGINE_FILESYSTEM_H
 #define CYPHER_ENGINE_FILESYSTEM_H
 
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherFileSystem_Error.h"
 #include "CypherFileSystem_Types.h"
