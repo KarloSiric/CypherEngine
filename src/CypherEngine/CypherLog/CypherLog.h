18af895356b53bc6a0f2d451a5999965137fc6f5
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:56 +0200

    Wrap pragma once guard in CypherLog.h

--- src/CypherEngine/CypherLog/CypherLog.h
+++ src/CypherEngine/CypherLog/CypherLog.h
@@ -19,7 +19,9 @@
 #ifndef CYPHER_ENGINE_LOG_H
 #define CYPHER_ENGINE_LOG_H
 
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherLog_Error.h"
 #include "CypherLog_Types.h"
