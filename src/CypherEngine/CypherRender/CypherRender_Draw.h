3087fb89f1b993fe761c8a387ce5f4cda15ae452
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:57 +0200

    Wrap pragma once guard in CypherRender_Draw.h

--- src/CypherEngine/CypherRender/CypherRender_Draw.h
+++ src/CypherEngine/CypherRender/CypherRender_Draw.h
@@ -19,7 +19,9 @@
 #ifndef CYPHER_ENGINE_RENDER_DRAW_H
 #define CYPHER_ENGINE_RENDER_DRAW_H
 
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherMath_Types.h"
 #include "CypherRender_Camera.h"
