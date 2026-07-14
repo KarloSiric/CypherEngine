7f55768f29525a5458531fdb13dcf5daf922ae23
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:51 +0200

    Wrap pragma once guard in CypherCommon_SystemInfo.h

--- src/CypherCommon/Tier0/CypherCommon_SystemInfo.h
+++ src/CypherCommon/Tier0/CypherCommon_SystemInfo.h
@@ -18,7 +18,9 @@
 
 #ifndef CYPHER_COMMON_TIER0_SYSTEMINFO_H
 #define CYPHER_COMMON_TIER0_SYSTEMINFO_H
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherCommon_BaseTypes.h"
 #include "CypherCommon_Defines.h"
