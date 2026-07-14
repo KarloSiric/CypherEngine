d4acfc0f2e749484db885b4b7d71a6f8a31b95d3
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:54 +0200

    Wrap pragma once guard in CypherCommon_NameServiceAddress.h

--- src/CypherCommon/Tier1/CypherCommon_NameServiceAddress.h
+++ src/CypherCommon/Tier1/CypherCommon_NameServiceAddress.h
@@ -18,7 +18,9 @@
 
 #ifndef CYPHER_COMMON_TIER1_NAMESERVICEADDRESS_H
 #define CYPHER_COMMON_TIER1_NAMESERVICEADDRESS_H
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 /*
 ================
