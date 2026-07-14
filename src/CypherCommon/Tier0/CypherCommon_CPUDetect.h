cb6baf74cd654179e2a6b9ea73c4f5ca1b4d9592
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:50 +0200

    Wrap pragma once guard in CypherCommon_CPUDetect.h

--- src/CypherCommon/Tier0/CypherCommon_CPUDetect.h
+++ src/CypherCommon/Tier0/CypherCommon_CPUDetect.h
@@ -18,7 +18,9 @@
 
 #ifndef CYPHER_COMMON_TIER0_CPUDETECT_H
 #define CYPHER_COMMON_TIER0_CPUDETECT_H
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherCommon_BaseTypes.h"
 #include "CypherCommon_Defines.h"
