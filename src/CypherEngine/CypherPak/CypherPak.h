caf68ad8c886bba3e5cebf5d8d0b1dbef1a82404
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-07-14 11:32:57 +0200

    Wrap pragma once guard in CypherPak.h

--- src/CypherEngine/CypherPak/CypherPak.h
+++ src/CypherEngine/CypherPak/CypherPak.h
@@ -19,7 +19,9 @@
 #ifndef CYPHER_ENGINE_PAK_H
 #define CYPHER_ENGINE_PAK_H
 
-#pragma once
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
 
 #include "CypherPak_Compression.h"
 #include "CypherPak_Error.h"
