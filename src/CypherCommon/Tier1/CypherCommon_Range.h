e5578155794cd1ca44bf9b3531ac1e9e85a9a486
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-08-09 10:25:02 +0200

    common: define Range API

--- src/CypherCommon/Tier1/CypherCommon_Range.h
+++ src/CypherCommon/Tier1/CypherCommon_Range.h
@@ -67,20 +67,37 @@ bool_t ByteRange_IsValid( byte_range_t range ) noexcept;
 CYPHER_NODISCARD CYPHER_COMMON_API
 usize ByteRange_End( byte_range_t range ) noexcept;
 
+CYPHER_NODISCARD CYPHER_COMMON_API
+bool_t ByteRange_ContainsOffset(
+    byte_range_t range,
+    usize iOffset ) noexcept;
+
+CYPHER_NODISCARD CYPHER_COMMON_API
+bool_t ByteRange_ContainsRange(
+    byte_range_t outer,
+    byte_range_t inner ) noexcept;
+
+CYPHER_NODISCARD CYPHER_COMMON_API
+byte_range_t ByteRange_Intersection(
+    byte_range_t rangeA,
+    byte_range_t rangeB ) noexcept;
+
 template <typename type_t>
-CYPHER_NODISCARD bool_t ValueRange_IsOrdered(
+CYPHER_NODISCARD constexpr bool_t ValueRange_IsOrdered(
     const value_range_t<type_t> &range ) noexcept;
 
 template <typename type_t>
-CYPHER_NODISCARD bool_t ValueRange_Contains(
+CYPHER_NODISCARD constexpr bool_t ValueRange_Contains(
     const value_range_t<type_t> &range,
     const type_t &value ) noexcept;
 
 template <typename type_t>
-CYPHER_NODISCARD type_t ValueRange_Clamp(
+CYPHER_NODISCARD constexpr type_t ValueRange_Clamp(
     const value_range_t<type_t> &range,
     const type_t &value ) noexcept;
 
 } // namespace cypher::common
+
+#include "CypherCommon_Range.inl"
 
 #endif // CYPHER_COMMON_TIER1_RANGE_H
