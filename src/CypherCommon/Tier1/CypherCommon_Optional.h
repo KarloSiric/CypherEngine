d2aba90907b1778d8fc04c27d83fa653d9d73975
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-08-09 10:25:02 +0200

    common: define Optional API

--- src/CypherCommon/Tier1/CypherCommon_Optional.h
+++ src/CypherCommon/Tier1/CypherCommon_Optional.h
@@ -28,6 +28,9 @@ namespace cypher::common
 
 template <typename type_t>
 struct optional_t {
+    static_assert( is_object_v<type_t>, "optional_t requires an object type." );
+    static_assert( !is_array_v<type_t>, "optional_t does not store array types." );
+
     optional_t() noexcept = default;
     ~optional_t() noexcept;
     optional_t( const optional_t & ) = delete;
@@ -61,6 +64,12 @@ CYPHER_NODISCARD type_t *Optional_EmplaceMove(
     optional_t<type_t> *pOptional,
     type_t &&value ) noexcept;
 
+// Replaces the current value by constructing type_t from forwarded arguments.
+template <typename type_t, typename... args_t>
+CYPHER_NODISCARD type_t *Optional_EmplaceArgs(
+    optional_t<type_t> *pOptional,
+    args_t &&...args ) noexcept;
+
 template <typename type_t>
 void Optional_Reset( optional_t<type_t> *pOptional ) noexcept;
 
@@ -70,5 +79,7 @@ CYPHER_NODISCARD bool_t Optional_Take(
     type_t *pValueOut ) noexcept;
 
 } // namespace cypher::common
+
+#include "CypherCommon_Optional.inl"
 
 #endif // CYPHER_COMMON_TIER1_OPTIONAL_H
