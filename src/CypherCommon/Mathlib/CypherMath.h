df43b8d8bea957cfae6cd5481ab042989bfdd6d5
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-08-11 19:39:04 +0200

    math: add public umbrella header

---
+++ src/CypherCommon/Mathlib/CypherMath.h
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  CypherEngine Source Code
+//  Copyright (c) 2026 Karlo Siric. All rights reserved.
+//
+//  File: src/CypherCommon/Mathlib/CypherMath.h
+//  Purpose: Provides the complete public include surface for CypherMath.
+//  Details: Consumers may include focused headers to reduce dependencies or use
+//           this umbrella when they require the complete runtime math foundation.
+//
+//  History:
+//  - Created by Karlo Siric on 2026-08-11
+//
+//  This file is proprietary and confidential. See LICENSE for details.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef CYPHER_COMMON_MATH_H
+#define CYPHER_COMMON_MATH_H
+#ifndef PRAGMA_ONCE
+    #pragma once
+#endif
+
+#include "CypherMath_Scalar.h"
+#include "CypherMath_Angle.h"
+#include "CypherMath_Vector2.h"
+#include "CypherMath_Vector3.h"
+#include "CypherMath_Vector4.h"
+#include "CypherMath_Quaternion.h"
+#include "CypherMath_Matrix3.h"
+#include "CypherMath_Matrix4.h"
+#include "CypherMath_Affine3.h"
+#include "CypherMath_Transform.h"
+#include "CypherMath_Plane.h"
+#include "CypherMath_Ray.h"
+#include "CypherMath_Bounds.h"
+#include "CypherMath_Sphere.h"
+#include "CypherMath_Triangle.h"
+#include "CypherMath_Frustum.h"
+#include "CypherMath_Intersection.h"
+#include "CypherMath_Batch.h"
+#include "CypherMath_Geometry2D.h"
+#include "CypherMath_Polygon.h"
+#include "CypherMath_Brush.h"
+#include "CypherMath_Snap.h"
+#include "CypherMath_Spline.h"
+#include "CypherMath_Numerics.h"
+#include "CypherMath_Viewport.h"
+#include "CypherMath_Clip.h"
+#include "CypherMath_UV.h"
+#include "CypherMath_FixedPoint.h"
+#include "CypherMath_Quantization.h"
+#include "CypherMath_Gizmo.h"
+
+#endif // CYPHER_COMMON_MATH_H
